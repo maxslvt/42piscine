343e0b47a6cbd51b0c279cceff6c72462abf7d65
Max Solet Levet <msolet-l@bess-f2r3s13.clusters.42paris.fr>
2023-07-13 21:53:13 +0200

    Add exs revision exam

--- revision exams/ft_atoi.c
+++ revision exams/ft_atoi.c
@@ -6,10 +6,21 @@
 /*   By: msolet-l <msolet-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 17:49:30 by msolet-l          #+#    #+#             */
-/*   Updated: 2023/07/13 17:50:29 by msolet-l         ###   ########.fr       */
+/*   Updated: 2023/07/13 21:52:54 by msolet-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+
+atoi = conversion d'une chaine de caractere en int
+exemple:
+in => "-++--152b69"(char*)
+out => -152(int)
+
+*/
+
+
+// regarde de quel type caractere il s'agit
 int	ft_char_flag(char c)
 {
 	if (c >= '0' && c <= '9')
@@ -33,18 +44,18 @@ int	ft_atoi(char *str)
 	nb = 0;
 	sign = 1;
 	idx = 0;
-	while (ft_char_flag(str[idx]) == 2)
+	while (ft_char_flag(str[idx]) == 2) // tant que les caracteres sont des espaces
 		idx++;
-	while (ft_char_flag(str[idx]) >= 3)
+	while (ft_char_flag(str[idx]) >= 3)// tant que les premiers caracteres sont de + ou des -
 	{
-		if (ft_char_flag(str[idx]) == 4)
-			sign *= -1;
+		if (ft_char_flag(str[idx]) == 4)// si il s'agit d'un -
+			sign *= -1;					// inverser le signe
 		idx++;
 	}
-	while (ft_char_flag(str[idx]) == 1)
+	while (ft_char_flag(str[idx]) == 1)// tant que les caracteres sont des chiffres
 	{
-		nb = nb * 10 + (str[idx] - 48);
+		nb = nb * 10 + (str[idx] - 48);// nb est multiplie par 10 et on ajoute le chiffre
 		idx++;
 	}
-	return (nb * sign);
+	return (nb * sign);// onretourne le nombre avec le signe
 }
--- revision exams/ft_range.c
+++ revision exams/ft_range.c
@@ -6,26 +6,34 @@
 /*   By: msolet-l <msolet-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 21:05:31 by msolet-l          #+#    #+#             */
-/*   Updated: 2023/07/13 21:13:13 by msolet-l         ###   ########.fr       */
+/*   Updated: 2023/07/13 21:52:56 by msolet-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+
+/*
+
+range = cree un tableau allant de min(inclus) a max(exclus)
+exemple:
+in => 2,5(int)(int)
+out => [2,3,4](int *)
+
+*/
 
 int	*ft_range(int min, int max)
 {
 	int	*tab;
 	int	i;
 
-	if (min >= max)
+	if (min >= max)// si min est superieur a max
 		return (NULL);
-	tab = (int*)malloc(sizeof(*tab) * (max - min));
-	i = 0;
-	while (min < max)
+	tab = (int*)malloc(sizeof(int) * (max - min));// cree un tableau de int de taille max - min
+	i = min;
+	while (i < max)// boucle qui parcours tab
 	{
-		tab[i] = min;
+		tab[i] = min;// rempli avec les nombre en commencant par min
 		i++;
-		min++;
 	}
 	return (tab);
 }
